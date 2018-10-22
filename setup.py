973d1ea849829f92f2454225cfb142733f30d8f5
Artem Selishchev <arselishev@gmail.com>
2018-10-22 20:29:23 +0300

    version: bump - 0.12.14.8

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cocaine",
-    version="0.12.14.6",
+    version="0.12.14.8",
     author="Anton Tyurin",
     author_email="noxiouz@yandex.ru",
     maintainer='Evgeny Safronov',
